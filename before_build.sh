ffd50fd6a39b70223e4a8a2a2889533799fa3d9a
Kevin.MX <aaawqw@live.cn>
2021-04-02 18:04:55 +0800

    Update before_build.sh

--- before_build.sh
+++ before_build.sh
@@ -4,3 +4,9 @@ cd /home/runner/immortalwrt
 # Enter your commands here, e.g.
 # echo "Start build!"
 make defconfig
+# Modify default IP
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+#Unblock AcFun in Adbyby
+sed -i '/yximg/d' package/lean/luci-app-adbyby-plus/root/etc/init.d/adbyby
+sed -i '/yximg/d' package/lean/luci-app-adbyby-plus/root/etc/init.d/adbyby

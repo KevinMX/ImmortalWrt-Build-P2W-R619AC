d48dabf1f150b9daef9b1cc0839733830b4edf91
Kevin.MX <aaawqw@live.cn>
2021-11-21 19:11:43 +0800

    Update before_build.sh

--- before_build.sh
+++ before_build.sh
@@ -8,5 +8,5 @@ make defconfig
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 #Unblock AcFun in Adbyby
-sed -i '/yximg/d' package/lean/luci-app-adbyby-plus/root/etc/init.d/adbyby
-sed -i '/yximg/d' package/lean/luci-app-adbyby-plus/root/etc/init.d/adbyby
+sed -i '/yximg/d' package/feeds/luci/luci-app-adbyby-plus/root/etc/init.d/adbyby
+sed -i '/yximg/d' package/feeds/luci/luci-app-adbyby-plus/root/etc/init.d/adbyby 
